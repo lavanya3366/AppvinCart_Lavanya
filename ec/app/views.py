70dfa9b0cb6824ef8cce102b3cca13d5689b6c1d
lavanya3366 <lavashri0303@gmail.com>
2024-02-06 18:08:46 +0530

    Appvin

---
+++ ec/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+# Create your views here.
+def home(request):
+    return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        return render(request,"app/category.html",locals())
